5455c7be5baf81725d88ccf6644052e6198205b6
CoderAssassins <116447575+CoderAssassins@users.noreply.github.com>
2023-09-10 10:45:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// Assignment 1
+// Creating array called furits
+let fruits = [];
+
+// inserting values in fruits array
+fruits.push('Apple');
+fruits.push('Banana');
+fruits.push('Orange');
+
+//Remove the first array from fruit
+fruits.splice(0,1);
+
+//add grape in the end of an array 
+fruits.push('Grape');
+
+//Update 2nd fruit in array to pear
+fruits[1] = 'Pear';
+
+// printing fruits
+console.log(fruits);
+
+
